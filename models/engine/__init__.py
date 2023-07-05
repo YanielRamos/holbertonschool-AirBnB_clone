1fb274edbdc803b27de8278d5d51f3fa55320702
YanielRamos <yanielramos0504@gmail.com>
2023-07-04 23:13:42 -0400

    adding file storage

---
+++ models/engine/__init__.py
@@ -0,0 +1,1 @@
+from models.engine.file_storege import FileStorage
---
+++ models/engine/file_storege.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module file storage class for AirBnB"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """this class serializes instances to a JSON file and
+    deserializes JSON file  to instances
+    __file_path: path to athe JSOn file
+    __objects: objects will be stored"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """return the dictionary"""
+        return self.__objects
+
+    def new(self, obj):
+        """adds a new object to __objects"""
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __object to the JSON file path"""
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dumps(my_dict, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects if
+        __file_path exist"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                for key, value in (json.loads(file)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
+            pass
