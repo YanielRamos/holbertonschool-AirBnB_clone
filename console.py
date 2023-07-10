84ccac924c59bdcda9d214bda75b852d818e0191
YanielRamos <yanielramos0504@gmail.com>
2023-07-09 21:07:34 -0400

    adding the module with storage

--- console.py
+++ console.py
@@ -12,6 +12,7 @@ from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
+from models import storage
 
 
 # Creating class HBNBCommand
